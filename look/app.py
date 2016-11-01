3aa0d25dc411d531252795c718cfafb787d510e6
Vasja Volin <vasja.volin@gmail.com>
2016-11-01 11:47:10 -0400

    add wiring for new image Item and Collection

--- look/app.py
+++ look/app.py
@@ -6,5 +6,8 @@ api = application = falcon.API()
 
 storage_path = '/tmp/look'
 
-images = images.Resource(storage_path)
-api.add_route('/images', images)
+image_collection = images.Collection(storage_path)
+image = images.Resource(storage_path)
+
+api.add_route('/images', image_collection)
+api.add_route('/images/{name}', image)
