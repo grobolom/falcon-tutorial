b65c622a90e96068b091a4b5334b85c5b8c86de9
Vasja Volin <vasja.volin@gmail.com>
2016-11-01 11:47:48 -0400

    fix typo referring to old class name

--- look/app.py
+++ look/app.py
@@ -7,7 +7,7 @@ api = application = falcon.API()
 storage_path = '/tmp/look'
 
 image_collection = images.Collection(storage_path)
-image = images.Resource(storage_path)
+image = images.Item(storage_path)
 
 api.add_route('/images', image_collection)
 api.add_route('/images/{name}', image)
