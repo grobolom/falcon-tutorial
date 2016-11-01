f5d9ee7ce242d1e74887e67e5dcb59079101713b
Vasja Volin <vasja.volin@gmail.com>
2016-11-01 11:43:42 -0400

    add a single item resource for pulling individual images

--- look/images.py
+++ look/images.py
@@ -24,3 +24,14 @@ class Collection(object):
 
         resp.status = falcon.HTTP_201
         resp.location = '/images/' + filename
+
+class Item(object):
+
+    def __init__(self, storage_path):
+        self.storage_path = storage_path
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_extension(name)[0]
+        image_path = os.path.join(self.storage_path, name)
+        resp.stream = open(image_path, 'rb')
+        resp.stream_len = os.path.getsize(image_path)
