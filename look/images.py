b7ec5b450496c3cb857df8ba45bc1c07c33ed440
Vasja Volin <vasja.volin@gmail.com>
2016-11-01 11:17:42 -0400

    add storage_path to the images resource

--- look/images.py
+++ look/images.py
@@ -2,6 +2,9 @@ import falcon
 
 class Resource(object):
 
+    def __init__(self, storage_path):
+        self.storage_path = storage_path
+
     def on_get(self, req, resp):
         resp.body = '{"message": "Hello World!"}'
         resp.status = falcon.HTTP_200
